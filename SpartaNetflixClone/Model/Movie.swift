6df38e0237fb0a1f1e412ecb1c2756c60f0755f8
youngjin <bryjna07@nate.com>
2024-12-26 11:32:12 +0900

    Feat: Model 구현

--- SpartaNetflixClone/Model/Movie.swift
+++ SpartaNetflixClone/Model/Movie.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int?
+    let title: String?
+    let posterPath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case posterPath = "poster_path"
+    }
+}
