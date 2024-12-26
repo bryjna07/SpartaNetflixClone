844e5f24c0bb01e4c5f28fc8f61b6dfa7e8d5471
youngjin <bryjna07@nate.com>
2024-12-26 11:38:58 +0900

    Feat: 코드베이스 설정

---
+++ SpartaNetflixClone/View/PosterCell.swift
@@ -0,0 +1,8 @@
+//
+//  PosterCell.swift
+//  SpartaNetflixClone
+//
+//  Created by t2023-m0033 on 12/26/24.
+//
+
+import Foundation
---
+++ SpartaNetflixClone/View/YoutubeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  YoutubeViewController.swift
+//  SpartaNetflixClone
+//
+//  Created by t2023-m0033 on 12/26/24.
+//
+
+import Foundation
